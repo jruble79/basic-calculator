408972b08d20525a318def520d35c822dd89b4fd
Jeff Ruble <jeff.ruble@gmail.com>
2021-04-01 11:38:44 -0400

    Button presses show in data-display

--- main.js
+++ main.js
@@ -3,12 +3,29 @@
 const dataDisplay = document.getElementById('data-display');
 const keyPad = document.getElementById('keypad');
 const button = document.getElementsByTagName('button');
+const display = document.getElementById('data-display');
+let keyPress;
+
+/////////////////////////////////////////////////////////////////
+// FUNCTIONS
+/////////////////////////////////////////////////////////////////
+
+
+
+/////////////////////////////////////////////////////////////////
+// PROCESSES
+/////////////////////////////////////////////////////////////////
+
+// Capture button presses
+// Sort into numbers and operators
+// Display button press value to data-display
 
 keyPad.addEventListener('click', e => {
     if (e.target.className == "operator") {
-        console.log(e.target.dataset.operation);
+        keyPress = e.target.dataset.operation;
     } else {
-        console.log(e.target.textContent);
+        keyPress = e.target.textContent;
     }
-
+    display.innerHTML = `<div id="display">${keyPress}</div>`;
+    return keyPress;
 });
