3cf255b73ee65c8931a979943f30dfbbaad51c07
Jeff Ruble <jeff.ruble@gmail.com>
2021-04-01 17:28:32 -0400

    Updates to display string values

--- main.js
+++ main.js
@@ -4,12 +4,36 @@ const dataDisplay = document.getElementById('data-display');
 const keyPad = document.getElementById('keypad');
 const button = document.getElementsByTagName('button');
 const display = document.getElementById('data-display');
-let keyPress;
+let keyPress = '';
+let numValA = '';
+let numValB = '';
+let methodCount = 0;
+let calculatedValue;
+
+
+const divide = (numValA, numValB) => numValA / numValB;
+const multiply = (numValA, numValB) => numValA * numValB;
+const subtract = (numValA, numValB) => numValA - numValB;
+const add = (numValA, numValB) => numValA + numValB;
+
 
 /////////////////////////////////////////////////////////////////
 // FUNCTIONS
 /////////////////////////////////////////////////////////////////
 
+// Displays keyPress data to data-display
+function displayData(input) {
+
+    if (input == "divide") {input = "÷"};
+    if (input == "multiply") {input = "&times;"};
+    if (input == "minus") {input = "-"};
+    if (input == "add") {input = "+"};
+    if (input == "toggle") {input = "&MinusPlus;"};
+    if (input == "clear") {input = ""};
+    if (input == "calculate") {input = ""};
+
+    display.innerHTML = `<div id="display">${input}</div>`;
+}
 
 
 /////////////////////////////////////////////////////////////////
@@ -17,15 +41,43 @@ let keyPress;
 /////////////////////////////////////////////////////////////////
 
 // Capture button presses
-// Sort into numbers and operators
-// Display button press value to data-display
 
 keyPad.addEventListener('click', e => {
+
+    // Sort into numbers and operators
+
     if (e.target.className == "operator") {
         keyPress = e.target.dataset.operation;
+        methodCount ++;
     } else {
         keyPress = e.target.textContent;
+        number = keyPress;
     }
-    display.innerHTML = `<div id="display">${keyPress}</div>`;
-    return keyPress;
+
+    // Display button press value to data-display
+
+    displayData(keyPress);
+
+    // Get first numVal
+
+    if (methodCount == 0 && e.target.className != "operator") {
+        numValA = numValA + number;
+        displayData(numValA);
+        number = '';
+    } 
+    
+    // Get second numVal
+    
+      else if (methodCount == 1 && e.target.className != "operator") {
+        numValB = numValB + number;
+        displayData(numValB);
+        number = '';
+    }
+
+    // Perform calculation
+
+      else if (methodCount == 2) {
+
+    }
+    
 });
