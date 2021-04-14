0dcf3fb5b0c5e1be2c6967bd243bc7c70c13cfb0
Jeff Ruble <jeff.ruble@gmail.com>
2021-04-14 14:00:36 -0400

    Refactors entirely. All buttons work and calculations performed correctly under ideal use case.

--- main.js
+++ main.js
@@ -1,160 +1,101 @@
 
-
 const display = document.getElementById('data-display');
-// const miniDisplay = document.getElementById('mini-display');
 const keyPad = document.getElementById('keypad');
+const toggleKey = document.getElementById('toggle');
+const clearKey = document.getElementById('clear');
+const equalsKey = document.getElementById('return');
 
-let numberInput;
-let numVal = 0;
-let numValA = 0;
-let numValB = 0;
-let result;
-let operation;
-let operationA;
-let operationB;
+let displayedNumber = '';
+let displayedOperation = '';
+let numberInput = '';
+let firstNumber = 0;
+let result = '';
+let operation = '';
 let i = 0;
 
-
-// Click to create an initial number value
-    // Display numbers as they are clicked
-    // Assign value to an initial-variable when operator is clicked
-
-// Click to choose an operation to perform
-    // Display operation symbol
-    // Assign operation to variable when clicked
-
-// Click to create a second number value
-    // Display numbers as they are clicked
-    // Assign value to a second-variable when operator is clicked
-
-// Click an operator to get result
-    // Create operation
-        // Get initial-variable
-        // Get operation variable
-        // Get second-variable
-    // Perform calculation
-    // Display result value
-    // Assign result value to the initial-variable
-
-// What does EQUALS do?
-    // Displays result of calculation
-    // Stores result as numValA
-        // If user clicks a number
-            // The current numValA is wiped
-            // New number becomes numValA
-    // Sets i = 0 ??;
-
-
-/////////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////
-
-// function displayData(input) {
-//     display.innerHTML = `<div id="display">${input}</div>`;
-// };
-
-// function getNumber() {
-//     if (i == 0) {
-//         numValA = numValA + numberInput;
-//         displayData(numValA);
-//     } else {
-//         numValB = numValB + numberInput;
-//         displayData(numValB);
-//     }
-// };
-
-// function prepareToOperate() {
-//     displayData(operation);
-//     if (i == 1) {
-//         operationA = operation;
-//     } else {
-//         operationB = operation;
-//         letsOperate(operationA);
-//     }
-// };
-
-// function letsOperate() {
-//     if (operationA == "divide") {
-//         numValA /= numValB;
-//     } else if (operationA == "multiply") {
-//         numValA *= numValB;
-//     } else if (operationA == "minus") {
-//         numValA -= numValB;
-//     } else if (operationA == "add") {
-//         numValA += numValB;
-//     };
-//     displayData(numValA);
-// };
-
-// keyPad.addEventListener('click', e => {
-//     if (e.target.className == "operator") {
-//         operation = e.target.dataset.operation;
-//         i++;
-//         prepareToOperate(operation);
-//     } else {
-//         numberInput = e.target.textContent;
-//         getNumber(numberInput);
-//     }
-// });
-
-/////////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////
-
 keyPad.addEventListener('click', e => {
-    if (e.target.className == "operator") {
-        operation = e.target.dataset.operation;
-        i++;
-        assignVariable();
-    } else if (e.target.className != "thing") {
+    if (e.target.className === 'operator') {
+        displayedOperation = e.target.dataset.action;
+        canWeCalculate();
+        getFirstOperation();
+        getFirstNumber();
+    } else if (e.target === clearKey) {
+        clearData();
+    } else if (e.target === toggleKey) {
+        toggleNumber();
+    } else if (e.target === equalsKey) {
+        calculateThis();
+    } else {
         numberInput = e.target.textContent;
-        buildNumberString(numberInput);
-    }
+        createNumber(numberInput);
+    };
 });
 
-// Adds clicks on number buttons to a number string
-function buildNumberString() {
-    numVal = numVal + numberInput;
-    numVal = +numVal; // Converts string to a number
-    displayData(numVal);
+function createNumber() { // CREATES NUMBER STRING
+    displayedNumber = displayedNumber + numberInput;
+    displayData(displayedNumber);
 };
 
-// Saves a number to a variable
-function assignVariable() {
-    if (i == 0) {
-        numValB = numVal;
-    } else if (i == 1) {
-        numValB = numVal;
-        numVal = '';
-    } else if (i == 2) {
-        numValA = numValB;
-        numValB = numVal;
+function getFirstNumber() { // ASSIGNS NUMBER IN DISPLAY TO VARIABLE
+    firstNumber = displayedNumber;
+    displayData(displayedNumber);
+    displayedNumber = '';
+};
+
+function getFirstOperation() { // ASSIGNS OPERATION IN DISPLAY TO VARIABLE
+    operation = displayedOperation;
+    displayedOperation = '';
+}
+
+function toggleNumber() {
+    displayedNumber = -1 * displayedNumber;
+    displayedNumber = parseFloat(displayedNumber);
+    displayData(displayedNumber);
+}
+
+function canWeCalculate() {
+    i++;
+    if (i === 2) {
+        i--;
+        calculateThis();
     }
 };
 
-function performCalculation() {
-    if (operation == "add") {
-        result = numValA + numValB;
-    } else if (operation  == "minus") {
-        result = numValA - numValB;
-    } else if (operation == "multiply") {
-        result = numValA * numValB;
-    } else if (operation == "divide") {
-        result = numValA / numValB;
+function calculateThis() {
+    if (operation === 'divide') {
+        result = parseFloat(firstNumber) / parseFloat(displayedNumber);
+    } else if (operation === 'multiply') {
+        result = parseFloat(firstNumber) * parseFloat(displayedNumber);
+    } else if (operation === 'minus') {
+        result = parseFloat(firstNumber) - parseFloat(displayedNumber);
+    } else if (operation === 'add') {
+        result = parseFloat(firstNumber) + parseFloat(displayedNumber);
     };
+
+    console.log(`
+    i=${i}
+    firstNumber=${firstNumber}
+    operation=${operation}
+    displayedNumber=${displayedNumber}
+    result=${result}`);
+    
     displayData(result);
-    i = 0;
-    numVal = result;
+    displayedNumber = result;
+    operation = '';
+
 };
 
-function displayData(input) {
+function displayData(input) { // DISPLAYS INPUT VALUE
     display.innerHTML = `<div id="display">${input}</div>`;
 };
 
 function clearData() {
-    numVal = 0;
-    numValA = 0;
-    numValB = 0;
+    firstNumber = 0;
+    displayedNumber = 0;
+    operation = '';
+    displayedOperation = '';
     i = 0;
-    displayData(numVal);
-}
+    displayData(displayedNumber);
+    displayedNumber = '';
+};
+
