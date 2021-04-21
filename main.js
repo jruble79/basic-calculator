5719a540ff19c4cf0c22b878032fb4804036760d
Jeff Ruble <jeff.ruble@gmail.com>
2021-04-21 14:16:26 -0400

    Adds basic functionality for background color change

--- main.js
+++ main.js
@@ -90,13 +90,42 @@ function displayData(input) { // DISPLAYS INPUT VALUE
 };
 
 function clearData() { // RESETS ALL VARIABLES TO INITIAL STATE
-    firstNumber = 0;
-    displayedNumber = 0;
-    operation = '';
-    displayedOperation = '';
-    result = 0;
-    i = 0;
-    displayData(displayedNumber);
     displayedNumber = '';
+    displayedOperation = '';
+    numberInput = '';
+    firstNumber = 0;
+    result = '';
+    operation = '';
+    i = 0;
+    displayData(0);
 };
+
+
+///////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////
+// COLOR THEMES ///////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////
+
+const background = document.getElementById('background');
+const themeSelect = document.getElementById('color-themes');
+// const colorThemes = document.getElementsByTagName('option');
+
+function output() {
+    // console.log(themeSelect.options[themeSelect.selectedIndex].textContent);
+    const colorTheme = themeSelect.options[themeSelect.selectedIndex].textContent;
+    if (colorTheme === '1111111') {
+        background.style.backgroundImage = 'linear-gradient(rgb(4, 75, 126), rgb(38, 2, 80))';
+    } else if (colorTheme === '2222222') {
+        background.style.backgroundImage = 'linear-gradient(darkgrey, black)';
+    } else if (colorTheme === '3333333') {
+        background.style.backgroundImage = 'linear-gradient(slategray, green)';
+    } else if (colorTheme === '4444444') {
+        background.style.backgroundImage = 'linear-gradient(red, black)';
+    } else if (colorTheme === '5555555') {
+        background.style.backgroundImage = 'linear-gradient(purple, blue, red)';
+    } else {
+        background.style.backgroundImage = 'linear-gradient(black, yellow, orange, green, blue, purple)';
+    }
+}
 
