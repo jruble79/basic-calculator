68beb3b40121ede1bd3c44f55f54abae5bf67d17
Jeff Ruble <jeff.ruble@gmail.com>
2021-08-04 16:58:35 -0400

    Refactors calculator functions

--- main.js
+++ main.js
@@ -5,71 +5,62 @@ const toggleKey = document.getElementById('toggle');
 const clearKey = document.getElementById('clear');
 const equalsKey = document.getElementById('return');
 
-let displayedNumber = '';
-let displayedOperation = '';
-let numberInput = '';
-let firstNumber = 0;
-let result = '';
+let number = 0;
+let firstNumber = '';
+let secondNumber = '';
 let operation = '';
+let result = '';
 
 keyPad.addEventListener('click', e => {
     if (e.target.className === 'operator') {
-        displayedOperation = e.target.dataset.action;
-        canWeCalculate(); // CHECKS IF THIS IS THE SECOND OPERATOR PUSH. CALCULATES IF TRUE
-        getFirstOperation(); // SAVES FIRST OPERATION TO VARIABLE
-        getFirstNumber(); // // ASSIGNS NUMBER IN DISPLAY TO VARIABLE
+        if (operation == '') {
+            operation = e.target.dataset.action;
+            firstNumber = number;
+            number = '';    
+        } else {
+            calculateThis();
+            operation = e.target.dataset.action;
+            firstNumber = result;
+            number = '';
+        }
     } else if (e.target === clearKey) {
-        clearData(); // RESETS ALL VARIABLES TO INITIAL STATE
+        clearData();
+        displayData(0);
     } else if (e.target === toggleKey) {
-        toggleNumber(); // ALTERNATES NUMBER FROM POSITIVE TO NEGATIVE
+        toggleNumber();
     } else if (e.target === equalsKey) {
-        calculateThis(); // PERFORMS CALCULATION
+        calculateThis();
     } else {
-        numberInput = e.target.textContent;
-        createNumber(numberInput); // CREATES NUMBER STRING
+        createNumber(e.target.textContent);
     };
 });
 
-function createNumber() { // CREATES NUMBER STRING
-    displayedNumber = displayedNumber + numberInput;
-    displayData(displayedNumber);
+function createNumber(numberInput) { // CREATES NUMBER STRING
+    number = number + numberInput;
+    displayData(number);
 };
-
-function getFirstNumber() { // ASSIGNS NUMBER IN DISPLAY TO VARIABLE
-    firstNumber = displayedNumber;
-    displayData(displayedNumber);
-    displayedNumber = '';
-};
-
-function getFirstOperation() { // ASSIGNS OPERATION IN DISPLAY TO VARIABLE
-    operation = displayedOperation;
-    displayedOperation = '';
-}
 
 function toggleNumber() { // ALTERNATES NUMBER FROM POSITIVE TO NEGATIVE
-    displayedNumber = parseFloat(displayedNumber * (-1));
-    displayData(displayedNumber);
+    number = number * (-1);
+    displayData(number);
 }
 
-function canWeCalculate() { // CHECKS IF THIS IS THE SECOND OPERATOR PUSH. CALCULATES IF TRUE
-    if (operation != '') {
-        calculateThis();
-    }
-};
-
 function calculateThis() { // PERFORMS CALCULATION
+    secondNumber = number;
+
     if (operation === 'divide') {
-        result = parseFloat(firstNumber) / parseFloat(displayedNumber);
+        result = parseFloat(firstNumber) / parseFloat(secondNumber);
     } else if (operation === 'multiply') {
-        result = parseFloat(firstNumber) * parseFloat(displayedNumber);
+        result = parseFloat(firstNumber) * parseFloat(secondNumber);
     } else if (operation === 'minus') {
-        result = parseFloat(firstNumber) - parseFloat(displayedNumber);
+        result = parseFloat(firstNumber) - parseFloat(secondNumber);
     } else if (operation === 'add') {
-        result = parseFloat(firstNumber) + parseFloat(displayedNumber);
+        result = parseFloat(firstNumber) + parseFloat(secondNumber);
     };
     
     displayData(result);
-    displayedNumber = result;
+    number = result;
+    secondNumber = '';
     operation = '';
 };
 
@@ -78,14 +69,11 @@ function displayData(input) { // DISPLAYS INPUT VALUE
 };
 
 function clearData() { // RESETS ALL VARIABLES TO INITIAL STATE
-    displayedNumber = '';
-    displayedOperation = '';
-    numberInput = '';
-    firstNumber = 0;
-    result = '';
+    number = '';
+    firstNumber = '';
+    secondNumber = '';
     operation = '';
-    i = 0;
-    displayData(0);
+    result = '';
 };
 
 
@@ -106,7 +94,7 @@ const dataDisplay = document.getElementById('data-display');
 const themes = [
     {
         'theme': 'Dusk',
-        '--background': 'linear-gradient(hsl(205, 100%, 25%), hsl(268, 100%, 25%)',
+        '--background': 'linear-gradient(hsl(205, 100%, 25%), hsl(268, 100%, 25%))',
         '--button-background': 'hsl(var(--white), .1)',
         '--text': 'hsl(var(--white), .5)',
         '--outline': '1px solid hsl(var(--white), .25)'
@@ -135,8 +123,6 @@ const themes = [
 ];
 
 function themeChange() {
-    let root = document.documentElement;
-    i = themeSelect.selectedIndex;
-    theme = themes[i];
-    for ( const prop in theme) { root.style.setProperty( `${prop}`, `${theme[prop]}` ) }
+    theme = themes[themeSelect.selectedIndex];
+    for ( const prop in theme ) { document.documentElement.style.setProperty( `${prop}`, `${theme[prop]}` ) }
 };
