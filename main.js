02e950e00329abe0182546fb65b7f3d977d4b0c4
Jeff Ruble <jeff.ruble@gmail.com>
2021-08-05 11:23:01 -0400

    Fixes toggle bug. Cleans up event listener through function calls to evaluateOperation and evaluateToggle

--- main.js
+++ main.js
@@ -13,27 +13,12 @@ let result = '';
 
 keyPad.addEventListener('click', e => {
     if (e.target.className === 'operator') {
-        if (operation == '') { // STARTS INITIAL CALCULATION
-            operation = e.target.dataset.action;
-            checkForZero();
-            firstNumber = number;
-            number = '';    
-        } else { // CONTINUES CALCULATION CHAIN
-            calculateThis(); // CALCULATES USING PREEXISTING OPERATION
-            firstNumber = result; // ASSIGNS RESULT OF CALCULATION AS NEW FIRST NUMBER
-            operation = e.target.dataset.action; // ASSIGNS NEW OPERATION FOR ANY NEXT CALCULATION
-            number = '';
-        }
+        evaluateOperation(e);
     } else if (e.target === clearKey) {
         clearData();
         displayData(0);
     } else if (e.target === toggleKey) {
-        if ( number == '0' || number == '') {
-            number = '-';
-            displayData('-');
-        } else {
-            toggleNumber();
-        };
+        evaluateToggle();
     } else if (e.target === equalsKey) {
         calculateThis();
     } else {
@@ -41,10 +26,34 @@ keyPad.addEventListener('click', e => {
     };
 });
 
+function evaluateOperation(e) {
+    if (operation == '') { // STARTS INITIAL CALCULATION
+        operation = e.target.dataset.action;
+        checkForZero();
+        firstNumber = number;
+        number = '';    
+    } else { // CONTINUES CALCULATION CHAIN
+        calculateThis(); // CALCULATES USING PREEXISTING OPERATION
+        firstNumber = result; // ASSIGNS RESULT OF CALCULATION AS NEW FIRST NUMBER
+        operation = e.target.dataset.action; // ASSIGNS NEW OPERATION FOR ANY NEXT CALCULATION
+        number = '';
+    };
+}
+
 function createNumber(numberInput) { // CREATES NUMBER STRING
     number = number + numberInput;
     displayData(number);
 };
+
+function evaluateToggle() {
+    if ( number == '0' || number == '') {
+        number = '-';
+        displayData('-');
+    } else {
+        toggleNumber();
+        displayData(number);
+    };
+}
 
 function toggleNumber() { // ALTERNATES NUMBER FROM POSITIVE TO NEGATIVE
     number = number * (-1);
